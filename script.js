e9ddb88a05691610b1dce6395250d1628283dbe5
MahmoudAbd0 <Mahmoud--Abdo@outlook.com>
2023-04-04 08:54:04 +0200

    Adding Gameover functions

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 let canvas = document.getElementsByTagName('canvas')[0]
 let context = canvas.getContext('2d')
+let gameOver = document.getElementById('gameover');
+let youLost = document.getElementById('youlost');
+let youWon = document.getElementById('youwon');
+let playAgain = document.getElementById('playagain');
+
+playAgain.addEventListener('click', function () {
+    location.reload();
+})
+
+function gameWonFun() {
+    gameOver.style.display = "flex";
+    youWon.style.display = "block";
+    playAgain.style.display = "block"
+}
+function gameLostFun() {
+    gameOver.style.backgroundColor = "rgba(0, 0, 0, .5)"
+    gameOver.style.display = "flex";
+    youLost.style.display = "block";
+    playAgain.style.display = "block"
+}
+gameWonFun();
